518b8825a67eb3b7c05c8c0767d67c4afa85bead
toshunigam <toshuchandra@gmail.com>
2021-02-05 22:47:07 +0530

    seed code added.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
+import userReducer from "./user-reducer";
 
 const rootReducer = combineReducers({
-
+    userList:userReducer
 })
 
 export default rootReducer;
---
+++ src/reducers/user-reducer.js
@@ -0,0 +1,10 @@
+const userReducer = (state,action) =>{
+    switch(action.type){
+        case "USER_LIST":
+            return action.pauload;
+        default:
+            return state;    
+    }
+}
+
+export default userReducer;
