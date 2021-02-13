e13313eca36a5931100c159fba9ba84655b62ef7
toshunigam <toshuchandra@gmail.com>
2021-02-13 23:28:52 +0530

    working into routing and custom hooks

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { Container } from 'react-bootstrap';
 import './App.css';
 import Navigation from './components/layouts/navigation';
 import UsersTable from './components/pages/users';
-// import { Header } from './components/layouts/header';
+import Register from './components/pages/register';
 
 function App() {
   return (
     <Container>
       <Navigation />
       <UsersTable />
+      <Register />
     </Container>
   );
 }
