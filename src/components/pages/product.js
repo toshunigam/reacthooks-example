6f2ed9f6a5acdc77ee70e21efbcf3df4505d2ad8
toshunigam <toshuchandra@gmail.com>
2021-05-09 19:16:27 +0530

    working into react bootstrap form

--- src/components/pages/product.js
+++ src/components/pages/product.js
@@ -1,19 +1,24 @@
 import React from "react";
-import { Row, Table } from "react-bootstrap";
+import { Alert, Row, Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { action_delete_product, action_edit_product } from "../../actions";
 import ProductForm from "./ProductForm";
 const Product = () => {
     const dispatch = useDispatch();
 
-    const product = useSelector(state=>state.productReduce);
-    console.log("LIST",product)
+    const product = useSelector(state => state.productReduce);
+    console.log("LIST", product)
     return (
         <Row>
-            <div style={{ width: "50%", margin: "0 auto" }}>
-                <h3>Product page</h3>
-                {/* <Nav.Link href="/addproduct">ADD Product</Nav.Link> */}
-                <ProductForm />
+            <div style={{ width: "100%", margin: "0 auto" }}>
+                <Alert variant="success">
+                        <Alert.Heading>Product Page</Alert.Heading>
+                        <p>CRUD Example using redux</p>
+                </Alert>
+                    
+                <div style={{ width: "50%", margin: "0 auto" }}>
+                    <ProductForm />
+                </div>
             </div>
             <Table striped bordered hover>
                 <thead>
@@ -26,17 +31,17 @@ const Product = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {product.length>0?product.map((item, index) => {
+                    {product.length > 0 ? product.map((item, index) => {
                         return (
                             <tr key={index}>
-                                <td>{index+1}</td>
+                                <td>{index + 1}</td>
                                 <td>{item.title}</td>
                                 <td>{item.description}</td>
                                 <td>{item.created}</td>
-                                <td>{/* <button onClick={()=>dispatch(action_edit_product(item.created))}>Edit</button> */}<button onClick={()=>dispatch(action_delete_product(item.created))}>Delete</button></td>
+                                <td>{/* <button onClick={()=>dispatch(action_edit_product(item.created))}>Edit</button> */}<button onClick={() => dispatch(action_delete_product(item.created))}>Delete</button></td>
                             </tr>
                         )
-                    }):<tr><td colSpan="5">No records</td></tr>}
+                    }) : <tr><td colSpan="5">No records</td></tr>}
                 </tbody>
             </Table>
         </Row>
