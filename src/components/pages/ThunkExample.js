580f0b32a56100c58fe6f8f98986e296760d898f
toshunigam <toshuchandra@gmail.com>
2021-05-15 21:01:28 +0530

    working into redux

--- src/components/pages/ThunkExample.js
+++ src/components/pages/ThunkExample.js
@@ -4,22 +4,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { addDOWN, addUP } from "../../actions";
 import logo from '../../logo.svg'
 
-const ThunkExample = ()=>{
-    const count = useSelector(state=>state.counterReducer);
+const ThunkExample = () => {
+    const count = useSelector(state => state.counterReducer);
     const dispatch = useDispatch();
-    return(
+    return (
         <div style={{ width: "97%", margin: "0 auto" }}>
-        <Alert variant="warning">
-            Redux thunk example
-        </Alert>
-            
-        <div style={{ width: "50%", margin: "0 auto" }}>
-            {count.counter} {count.loading===true?<img className="App-logo" src={logo} width="50" height="50" alt="logo" />:''}
-            <hr />
-            <button onClick={()=>dispatch(addUP())}>Add UP</button>
-            <button onClick={()=>dispatch(addDOWN())}>Add Down</button>
+            <Alert variant="warning">
+                Redux thunk example
+            </Alert>
+
+            <div style={{ width: "50%", margin: "0 auto" }}>
+                {count.counter} {count.loading === true ? <img className="App-logo" src={logo} width="50" height="50" alt="logo" /> : ''}
+                <hr />
+                <button onClick={() => dispatch(addUP())}>Add UP</button>
+                <button onClick={() => dispatch(addDOWN())}>Add Down</button>
+            </div>
         </div>
-    </div>
 
     )
 }
--- src/reducers/contact-reducer.js
+++ src/reducers/contact-reducer.js
@@ -1,14 +1,14 @@
 const contactReducer = (state=[],action)=>{
     switch(action.type){
         case 'ADD_CONTACT':
-            return state.concat(action.payload)
+            return state.concat(action.payload);
         case 'DELETE_CONTACT':
             return state.filter(item=>{return item.id!==action.payload});
         case 'EDIT_CONTACT':
             const array = state.map(item=>{
                 return item.id===action.payload?item.editable=true:item.editable=state
             })
-            console.log(array)
+            // console.log(array)
             let arr = array.filter(item=>{return typeof item!=='boolean'?item:null});
             console.log(arr)
             return arr[0];
